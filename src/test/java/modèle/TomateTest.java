398f065224c379a55155a5d7b58b487435b0b42c
victor Simonet <102629039+simonet4@users.noreply.github.com>
2025-06-10 18:09:30 +0200

    test

---
+++ src/test/java/modèle/TomateTest.java
@@ -0,0 +1,14 @@
+package modèle;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TomateTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
