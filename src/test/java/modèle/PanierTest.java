6c23c23ad18d1b3b9dcd6ce6dacda37f49d0d41e
Elliot Isken--Andersen Nexö <122434767+Elliot-Isken-Andersen-Nexo@users.noreply.github.com>
2025-06-12 08:44:03 +0200

    début PanierTest

--- src/test/java/modèle/PanierTest.java
+++ src/test/java/modèle/PanierTest.java
@@ -11,12 +11,22 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class PanierTest {
-
+	
     private static Panier panier;
+    private static Tomate tomateEnStock;
+    private static Tomate tomateEnRupture;
 
     @BeforeClass
     public static void setUp() throws Exception {
-    	Tomates tomates = 
-        this.panier = new Panier()
+        Tomates tomates = OutilsBaseDonneesTomates.générationBaseDeTomates("src/main/resources/data/tomates.json");
+        tomateEnRupture = tomates.getTomate("Tomate Chair de Boeuf « Beefsteak »");
+        tomateEnStock = tomates.getTomate("Tomate Joie de la Table");
+        panier  = new Panier();
+    }
+    
+    @Test
+    public void testAjoutTomate() {
+        panier.ajouterTomate(tomateEnStock, 1);
+        assertEquals(4.95F, panier.total(), 0.1F);
     }
 }
